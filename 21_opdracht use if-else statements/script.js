4707e74a49718f45e787ac23db0a14288af827d6
Bernadette92 <79193891+Bernadette92@users.noreply.github.com>
2021-03-10 15:22:46 +0100

    Add files via upload

---
+++ 21_opdracht use if-else statements/script.js
@@ -0,0 +1,26 @@
+const myAge = 18;
+
+if (myAge > 17) {
+    console.log("Je mag naar binnen, veel plezier!");
+}
+else {
+    console.log("Helaas, je mag niet naar binnen :(");
+}
+
+const isFemale = true;
+if (isFemale) {
+    console.log("Je bent een vrouw.");
+}
+else {
+    console.log("Je bent geen vrouw.");
+}
+
+const driverStatus = "bob";
+if (driverStatus == "bob") {
+    console.log("Je mag rijden");
+}
+else {
+    console.log("Je hebt gedronken dus je mag niet rijden");
+}
+
+
